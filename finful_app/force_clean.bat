534c979f39943b807a5d5bf00becf1d0906faad6
Duy <duypq2610@gmail.com>
2025-12-11 00:15:02 +0700

    remove rive

---
+++ finful_app/force_clean.bat
@@ -0,0 +1,15 @@
+@echo off
+echo 💥 Force killing Gradle + Android Studio...
+taskkill /F /IM studio64.exe >nul 2>&1
+taskkill /F /IM java.exe >nul 2>&1
+taskkill /F /IM gradle* >nul 2>&1
+
+echo 🧹 Deleting all /build folders...
+for /d /r %%d in (build) do (
+    if exist "%%d" (
+        echo   deleting %%d
+        rd /s /q "%%d"
+    )
+)
+
+echo ✅
